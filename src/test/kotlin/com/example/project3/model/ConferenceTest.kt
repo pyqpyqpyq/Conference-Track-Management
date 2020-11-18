a3b1185f9bea416213fdf8a05670f680b513e72f
“pyqpyqpyq” <alwayswannaflyx@gmail.com>
2020-11-18 13:40:44 +0800

    [pyq] remove the method and test of isLonger which is no need anymore

--- src/test/kotlin/com/example/project3/model/ConferenceTest.kt
+++ src/test/kotlin/com/example/project3/model/ConferenceTest.kt
@@ -13,25 +13,6 @@ class ConferenceTest {
         Assertions.assertEquals(conference.getTrack(1), Track(1))
     }
     @Test
-    fun `should be able to compare the length of two talks in minutes by function isLonger `() {
-        val input = Input()
-        val conference = Conference()
-        val string1 = "Writing Fast Tests Against Enterprise Rails 60min"
-        val string2 = "Overdoing it in Python 45min"
-        Assertions.assertEquals(conference.isLonger(input.transferStringToTalk(string1), input.transferStringToTalk(string2)), true)
-        Assertions.assertEquals(conference.isLonger(input.transferStringToTalk(string2), input.transferStringToTalk(string1)), false)
-        Assertions.assertEquals(conference.isLonger(input.transferStringToTalk(string1), input.transferStringToTalk(string1)), false)
-    }
-    @Test
-    fun `should be able to compare the length of two talks one in minutes and another in lightning by function isLonger `() {
-        val input = Input()
-        val conference = Conference()
-        val string1 = "Writing Fast Tests Against Enterprise Rails 60min"
-        val string2 = "Rails for Python Developers lightning"
-        Assertions.assertEquals(conference.isLonger(input.transferStringToTalk(string1), input.transferStringToTalk(string2)), true)
-        Assertions.assertEquals(conference.isLonger(input.transferStringToTalk(string2), input.transferStringToTalk(string1)), false)
-    }
-    @Test
     fun `should be able to rank the list of talks by their length in descending, so the shortest event should be in the last`() {
         val input = Input()
         val conference = Conference()
