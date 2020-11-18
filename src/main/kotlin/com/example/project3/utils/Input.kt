64f85f1ba6dc71f7a3faf36b0245af7c4b0baa21
“pyqpyqpyq” <alwayswannaflyx@gmail.com>
2020-11-18 11:32:14 +0800

    [pyq] refactoring the read method in the input class to read input txt file to the List of Talk.

--- src/main/kotlin/com/example/project3/utils/Input.kt
+++ src/main/kotlin/com/example/project3/utils/Input.kt
@@ -6,11 +6,11 @@ import java.io.File
 import java.io.InputStream
 
 class Input {
-    fun read(): MutableList<String> {
+    fun read(): MutableList<Talk> {
         val inputStream: InputStream = File(Constant.INPUT_FILE_PATH).inputStream()
-        val lineList = mutableListOf<String>()
-        inputStream.bufferedReader().forEachLine { lineList.add(it) }
-        return lineList
+        val talkList = mutableListOf<Talk>()
+        inputStream.bufferedReader().forEachLine { talkList.add(transferStringToTalk(it)) }
+        return talkList
     }
 
     fun transferStringToDuration(input: String): Duration {
