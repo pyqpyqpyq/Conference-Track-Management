5cd5ef933949dfd7859ca9bd9594d1fdc1ba1857
“pyqpyqpyq” <alwayswannaflyx@gmail.com>
2020-11-19 16:37:03 +0800

    [pyq] recover the mis deleted class "ConferenceManagerTest"

--- src/main/kotlin/com/example/conference/slots/Slot.kt
+++ src/main/kotlin/com/example/conference/slots/Slot.kt
@@ -10,12 +10,14 @@ abstract class Slot {
     private var events = mutableListOf<Event>()
     abstract var restLength: Int
     fun arrange(talk: Talk): Boolean? {
-        if (talk.duration.toMinutes() <= restLength) {
+        return if (talk.duration.toMinutes() <= restLength) {
             restLength -= talk.duration.toMinutes()
             events.add(talk)
             addedTime.add(addedTime.last().plusMinutes(talk.duration.toMinutes().toLong()))
-            return true
-        } else { return false }
+            true
+        } else {
+            false
+        }
     }
     fun getEvent(Id: Int): Event {
         return events[Id]
