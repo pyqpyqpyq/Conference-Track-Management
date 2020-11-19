7ce6cedab35f9b51f1a30cd0d10f2c69f22094e0
“pyqpyqpyq” <alwayswannaflyx@gmail.com>
2020-11-19 14:56:11 +0800

    [pyq] add test should be able to get tract by its id each track has unique id

--- src/main/kotlin/com/example/conference/Track.kt
+++ src/main/kotlin/com/example/conference/Track.kt
@@ -3,15 +3,14 @@ package com.example.conference
 import com.example.conference.slots.Afternoon
 import com.example.conference.slots.Morning
 
-class Track(var id: Int) {
-    var morning = Morning()
-    var afternoon = Afternoon()
+class Track(val id: Int) {
+    val morning = Morning()
+    val afternoon = Afternoon()
     override fun equals(other: Any?): Boolean {
         return when (other) {
             !is Track -> false
             else ->
-                this === other ||
-                    id == other.id
+                id == other.id
         }
     }
 }
--- src/test/kotlin/com/example/conference/TrackTest.kt
+++ src/test/kotlin/com/example/conference/TrackTest.kt
@@ -7,6 +7,11 @@ import org.junit.jupiter.api.Test
 
 class TrackTest {
     @Test
+    fun `should be able to get tract by its id each track has unique id`() {
+        val track1 = Track(1)
+        Assertions.assertEquals(track1.id, 1)
+    }
+    @Test
     fun `should be able to create the object track (by id)which has the morning time slot, and it can put event inside`() {
         val name = "Writing Fast Tests Against Enterprise Rails"
         val duration = 60
@@ -24,5 +29,16 @@ class TrackTest {
         val track = Track(1)
         track.afternoon.arrange(event)
         Assertions.assertEquals(track.afternoon.restLength, 180)
+    }
+    @Test
+    fun `should be different if the id of the track is different`() {
+        val track1 = Track(1)
+        val track2 = Track(2)
+        Assertions.assertNotEquals(track1, track2)
+    }
+    @Test
+    fun `should be  different if compare track with other class`() {
+        val track1 = Track(1)
+        Assertions.assertNotEquals(track1, 1)
     }
 }
