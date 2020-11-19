6e4ce0936431748b188d49de2029750ed0a74b88
“pyqpyqpyq” <alwayswannaflyx@gmail.com>
2020-11-19 13:29:14 +0800

    [pyq] removed the unnecessary data class for track which did not follow the minimize implementation in the tdd

--- src/main/kotlin/com/example/conference/Track.kt
+++ src/main/kotlin/com/example/conference/Track.kt
@@ -3,7 +3,15 @@ package com.example.conference
 import com.example.conference.slots.Afternoon
 import com.example.conference.slots.Morning
 
-data class Track(var id: Int) {
+class Track(var id: Int) {
     var morning = Morning()
     var afternoon = Afternoon()
+    override fun equals(other: Any?): Boolean {
+        return when (other) {
+            !is Track -> false
+            else ->
+                this === other ||
+                    id == other.id
+        }
+    }
 }
